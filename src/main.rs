03589d5f5f09ecff08f4be99f88654a4040ab1a5
William Budington <bill@eff.org>
2022-03-14 13:51:22 -0700

    Update POC to print digest of X509 certificate

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
-use cryptographic_message_syntax::SignedData;
+use std::fmt::Write;
 use std::fs;
 
+use cryptographic_message_syntax::SignedData;
+use openssl::x509::X509;
+use openssl::hash::MessageDigest;
+use x509_certificate::certificate::CapturedX509Certificate;
+
 fn main() {
-    let ciarang_bytes = fs::read("rsa-files/CIARANG.RSA").unwrap();
-    let one_bytes = fs::read("rsa-files/1.RSA").unwrap();
-    assert!(SignedData::parse_ber(&ciarang_bytes).is_ok());
-    assert!(SignedData::parse_ber(&one_bytes).is_ok());
+    print_cert_fingerprint("rsa-files/CIARANG.RSA");
+    print_cert_fingerprint("rsa-files/1.RSA");
+}
+
+fn print_cert_fingerprint(file: &str){
+    let bytes = fs::read(file).unwrap();
+    let cert = SignedData::parse_ber(&bytes).unwrap().certificates().collect::<Vec<&CapturedX509Certificate>>()[0].clone();
+    let x509 = X509::from_der(&cert.encode_ber().unwrap()).unwrap();
+    let cert_fingerprint = x509.digest(MessageDigest::from_name("sha256").unwrap()).unwrap();
+
+    let mut s = String::new();
+    for byte in cert_fingerprint.iter() {
+        write!(&mut s, "{:02X?}", byte).expect("Unable to write");
+    }
+    println!("{}", s);
 }
